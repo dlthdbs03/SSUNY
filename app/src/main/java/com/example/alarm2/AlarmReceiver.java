a666f72e7398a0ee6a7cf4a5946dfdccbe402443
dlthdbs03 <dlsth13@gmail.com>
2023-05-26 09:18:57 +0900

    알람 여러 개 등록할 수 있도록 기능 추가

--- app/src/main/java/com/example/alarm2/AlarmReceiver.java
+++ app/src/main/java/com/example/alarm2/AlarmReceiver.java
@@ -7,7 +7,7 @@ import android.os.Build;
 import android.os.PowerManager;
 import android.widget.Toast;
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver {//
     private PowerManager.WakeLock wakeLock;
 
     @Override
